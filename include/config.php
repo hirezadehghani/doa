9a75626d581f3fd8da6631221618d20d637ffe67
Reza Dehghani <75562501+reza-dehghani@users.noreply.github.com>
2021-09-06 13:10:36 +0430

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,6 +1,6 @@
 <?php
 define("DSN", "mysql:host=localhost;dbname=doa;charset=utf8");
-define("DB_USER", "root");
-define("DB_PASS", "o1CjXoBdb2$.");
+define("DB_USER", "yourdatabaseusername");
+define("DB_PASS", "yourdatabasepassword");
 $db = new PDO(DSN, DB_USER, DB_PASS);
 ?>
